583e35e09a24bde23d46e5573942b8a05cd28649
Joe Hong <joe.hong@staxpayments.com>
2026-01-05 16:54:48 -0500

    add Mars alias for darwin-rebuild commands

--- hosts/Mars/home.nix
+++ hosts/Mars/home.nix
@@ -11,6 +11,8 @@
 
   xdg.configFile."zsh/zsh_aliases_mars".text = ''
     alias vi = nvim
+    alias ndb = "sudo darwin-rebuild build --flake .#Mars
+    alias nds = "sudo darwin-rebuild switch --flake .#Mars
   '';
 }
 
