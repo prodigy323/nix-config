d90cf7461789c1e44cd28947abdef6f763680ee9
Joe Hong <jhng323@gmail.com>
2026-04-11 11:34:47 -0400

    set homebrew to uninstall packages if removed

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -25,20 +25,20 @@
     ];
 
     casks = [
-      "brave-browser"
+      #"brave-browser"
       #"firefox"
       "font-meslo-lg-nerd-font"
       "font-symbols-only-nerd-font"
       "ghostty"
       #"google-chrome"
       #"iterm2"
-      "raycast"
+      #"raycast"
       "shottr"
     ];
 
     onActivation = {
       upgrade = true;
-      cleanup = "none"; # switch to "uninstall" later
+      cleanup = "uninstall"; # switch to "none" or "uninstall"
     };
   };
 }
