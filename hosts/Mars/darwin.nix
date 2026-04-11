76ad5992994da57992873dccdd0e17e0f7e5c21a
Joe Hong <joehong@Joes-MacBook-Pro.local>
2026-04-11 10:39:43 -0400

    update Mars hostname

--- hosts/Mars/darwin.nix
+++ hosts/Mars/darwin.nix
@@ -2,13 +2,13 @@
 
 {
   system.primaryUser = username;
-  networking.hostName = "Joes-MacBook-Pro";
+  networking.hostName = "Joes-MacBook-Pro.local";
 
   homebrew = {
     brews = [
-      "aws-cdk"
-      "aws-vault"
-      "awscli"
+      # "aws-cdk"
+      # "aws-vault"
+      # "awscli"
     ];
 
     casks = [
